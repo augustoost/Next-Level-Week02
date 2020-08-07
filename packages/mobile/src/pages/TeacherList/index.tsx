328f54b04c7fef6c2dba6f5c1c10a3c79e00d1ea
Augusto Ostapechen <augusto_ostapechen@hotmail.com>
2020-08-07 14:53:46 -0300

    Added functionality to change favorites state.

--- packages/mobile/src/pages/TeacherList/index.tsx
+++ packages/mobile/src/pages/TeacherList/index.tsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { View, Text, ScrollView, TextInput } from 'react-native';
 import { BorderlessButton, RectButton } from 'react-native-gesture-handler';
 import { Feather } from '@expo/vector-icons';
+import AsyncStorage from '@react-native-community/async-storage';
+
 import api from '../../services/api';
 
 import PageHeader from '../../components/PageHeader';
@@ -11,17 +13,34 @@ import styles from './styles';
 
 function TeacherList() {
   const [isFiltersVisible, setIsFiltersVisible] = useState(false);
+  const [favorites, setFavorites] = useState<number[]>([]);
   const [teachers, setTeachers] = useState([]);
 
   const [subject, setSubject] = useState('');
   const [week_day, setWeekDay] = useState('');
   const [time, setTime] = useState('');
 
+  function loadFavorites() {
+    AsyncStorage.getItem('favorites').then(response => {
+      if ( response ){
+        const favoritedTeachers = JSON.parse(response);
+        const favoritedTeachersIds = 
+          favoritedTeachers.map((teacher: ITeacher) => {
+            return teacher.id
+        });
+
+        setFavorites(favoritedTeachersIds);
+      }
+    });
+  }
+
   function handleToggleFiltersVisible() {
     setIsFiltersVisible(!isFiltersVisible);
   }
 
   async function handleFilterSubmit(){
+    loadFavorites();
+
     const response = await api.get('classes', {
       params: {
         subject,
@@ -98,7 +117,13 @@ function TeacherList() {
         }}
       >
         {teachers.map((teacher: ITeacher) => {
-          return <TeacherItem key={teacher.id} teacher={teacher} />
+          return (
+            <TeacherItem 
+              key={teacher.id} 
+              teacher={teacher} 
+              favorited={favorites.includes(teacher.id)}
+            />
+          );
         })}
       </ScrollView>
     </View>
