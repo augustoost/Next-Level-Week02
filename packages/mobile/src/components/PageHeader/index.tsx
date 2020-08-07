36a8f15efdb33f8abc16cd1644ced394d0635a9e
Augusto Ostapechen <augusto_ostapechen@hotmail.com>
2020-08-07 12:35:08 -0300

    Added Proffy filters

--- packages/mobile/src/components/PageHeader/index.tsx
+++ packages/mobile/src/components/PageHeader/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { View, Image, Text } from 'react-native';
 import { BorderlessButton } from 'react-native-gesture-handler';
 
@@ -10,9 +10,10 @@ import { useNavigation } from '@react-navigation/native';
 
 interface IPageHeaderProps {
   title: string;
+  headerRight?: ReactNode;
 }
 
-const PageHeader: React.FC<IPageHeaderProps> = ({ title }) => {
+const PageHeader: React.FC<IPageHeaderProps> = ({ title, headerRight, children }) => {
   const { navigate } = useNavigation();
 
   function handleGoBack() {
@@ -28,8 +29,14 @@ const PageHeader: React.FC<IPageHeaderProps> = ({ title }) => {
 
         <Image source={logoImg} resizeMode="contain" />
       </View>
+      
+      <View style={styles.header}>
+        <Text style={styles.title}>{title}</Text>
+        {headerRight}
+      </View>
 
-      <Text style={styles.title}>{title}</Text>
+      { children }
+      
     </View>
   );
 }
